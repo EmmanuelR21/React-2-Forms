e5d3670d411502f676dfe2c5224919d7cdda12a9
Jocsan Rodriguez <jasub270@gmail.com>
2025-06-25 15:28:20 -0400

    bookList

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+// Book Object: {
+//     title: string,
+//     author: string,
+//     ratings: Array of floats,
+//     release date: "string"
+// }
+
+import React, {useState} from "react"; 
 import { createRoot } from "react-dom/client";
 import "./style.css";
 import AddBook from "./components/AddBook";
 import BookList from "./components/BookList";
 
 const App = () => {
+  const [books, setBooks] = useState([{
+title: "Book1",
+author: "author1",
+ratings: [2.5, 5, 3, 3],
+releaseDate: "December 2025",
+},
+{
+title: "Book2",
+author: "author2",
+ratings: [3.5, 4, 3.5, 3],
+releaseDate: "November 2025",
+},
+]);
+
   return (
     <div className="app">
       <h1 className="title">React Forms! 📝</h1>
       <AddBook />
-      <BookList />
+      <BookList books={books}/>
+      <div>
+        <button></button>
+      </div>
     </div>
   );
 };
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-const BookList = () => {
-  return <div>BookList</div>;
+const BookList = ({books}) => {
+const [searchedBook, setSearchedBook] = useState([]);
+ console.log(searchedBook);
+   const handleSubmit = (event) => {
+    event.preventDefault();
+    const titleInput = event.target[0].value;
+    console.log("title:", titleInput);
+
+    for(let i = 0; i < books.length; i++){
+      if(books[i].title.toLowerCase().includes(titleInput.toLowerCase())){
+          setSearchedBook([...searchedBook, books[i]]);
+      }
+    }  
+   
+   }
+
+  //  const clearBar = () => {
+  //   setTitleInput("");
+  //  }
+
+  return (
+    <>
+    <div>Book List</div>
+       <form onSubmit={handleSubmit}>
+         <input 
+         name ="title"
+         type ="text"
+         required
+         placeholder="Title"
+         />
+         <button className="buttom" type="submit"> search </button>
+       </form>
+    </>
+  );
 };
 
 export default BookList;

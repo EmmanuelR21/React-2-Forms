18b79b5db0019665a81241bfbcbc0720aeee1a33
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-06-25 15:47:22 -0400

    Format and clean up code in App and BookList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,30 @@
-// Book Object: {
-//     title: string,
-//     author: string,
-//     ratings: Array of floats,
-//     release date: "string"
-// }
-
-import React, {useState} from "react"; 
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 import AddBook from "./components/AddBook";
 import BookList from "./components/BookList";
 
 const App = () => {
-  const [books, setBooks] = useState([{
-title: "Book1",
-author: "author1",
-ratings: [2.5, 5, 3, 3],
-releaseDate: "December 2025",
-},
-{
-title: "Book2",
-author: "author2",
-ratings: [3.5, 4, 3.5, 3],
-releaseDate: "November 2025",
-},
-]);
+  const [books, setBooks] = useState([
+    {
+      title: "Book1",
+      author: "author1",
+      ratings: [2.5, 5, 3, 3],
+      releaseDate: "December 2025",
+    },
+    {
+      title: "Book2",
+      author: "author2",
+      ratings: [3.5, 4, 3.5, 3],
+      releaseDate: "November 2025",
+    },
+  ]);
 
   return (
     <div className="app">
       <h1 className="title">React Forms! 📝</h1>
       <AddBook />
-      <BookList books={books}/>
-      <div>
-        <button></button>
-      </div>
+      <BookList books={books} />
     </div>
   );
 };
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,37 +1,51 @@
 import React, { useState } from "react";
+import BookCard from "./BookCard";
 
-const BookList = ({books}) => {
-const [searchedBook, setSearchedBook] = useState([]);
- console.log(searchedBook);
-   const handleSubmit = (event) => {
+const BookList = ({ books }) => {
+  const [searchedBook, setSearchedBook] = useState([]);
+  console.log(searchedBook);
+  const handleSubmit = (event) => {
     event.preventDefault();
     const titleInput = event.target[0].value;
     console.log("title:", titleInput);
 
-    for(let i = 0; i < books.length; i++){
-      if(books[i].title.toLowerCase().includes(titleInput.toLowerCase())){
-          setSearchedBook([...searchedBook, books[i]]);
+    for (let i = 0; i < books.length; i++) {
+      if (books[i].title.toLowerCase().includes(titleInput.toLowerCase())) {
+        setSearchedBook([...searchedBook, books[i]]);
       }
-    }  
-   
-   }
-
-  //  const clearBar = () => {
-  //   setTitleInput("");
-  //  }
+    }
+  };
 
   return (
     <>
-    <div>Book List</div>
-       <form onSubmit={handleSubmit}>
-         <input 
-         name ="title"
-         type ="text"
-         required
-         placeholder="Title"
-         />
-         <button className="buttom" type="submit"> search </button>
-       </form>
+      <div>Book List</div>
+      <form onSubmit={handleSubmit}>
+        <input name="title" type="text" required placeholder="Title" />
+        <button className="buttom" type="submit">
+          search
+        </button>
+      </form>
+      {searchedBook.length !== 0
+        ? searchedBook.map((book) => {
+            return (
+              <BookCard
+                title={book.title}
+                author={book.author}
+                ratings={book.ratings}
+                releaseDate={book.releaseDate}
+              />
+            );
+          })
+        : books.map((book) => {
+            return (
+              <BookCard
+                title={book.title}
+                author={book.author}
+                ratings={book.ratings}
+                releaseDate={book.releaseDate}
+              />
+            );
+          })}
     </>
   );
 };
